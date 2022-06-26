6ac0b203319ab1b2203222b713837985487ab576
alonspinner <alonspinner@gmail.com>
2022-06-26 14:59:26 +0300

    fixed conditions in move at 04

--- tests/05_formulatePlanner_multirobot.py
+++ tests/05_formulatePlanner_multirobot.py
@@ -1,7 +1,7 @@
 import unified_planning
 from unified_planning.shortcuts import UserType, BoolType, IntType, Int,\
         Fluent, DurativeAction, InstantaneousAction, SimulatedEffect, Problem, Object, OneshotPlanner,\
-        StartTiming, EndTiming,  GE, Or, Not
+        StartTiming, EndTiming, GE, Equals, Or
 unified_planning.shortcuts.get_env().credits_stream = None #removes the printing planners credits 
 
 # --------------------------------------------- Define problem variables and actions
@@ -12,6 +12,9 @@ MOVE_TIME = 1.0
 PICKUP_TIME = 1.0
 DROP_TIME = 1.0
 CHARGING_TIME = 1.0
+
+def dist2chargeuse(dist):
+        return 2 * dist
 
 
 #problem types ~ objectsv
@@ -38,21 +41,21 @@ l_to = move.parameter('l_to')
 move.set_fixed_duration(MOVE_TIME)
 move.add_condition(StartTiming(),Or(is_connected(l_from, l_to), \
                                     is_connected(l_to, l_from)))
-move.add_condition(StartTiming(),GE(charge(r),LOCATION_DISTANCE))
+move.add_condition(StartTiming(),GE(charge(r),(LOCATION_DISTANCE)))
 move.add_condition(StartTiming(), robot_at(r, l_from))
-move.add_condition(EndTiming(), Not(is_occupied(l_to))) #at end, l_to is free
+move.add_condition(EndTiming(), is_occupied(l_to))
 move.add_effect(StartTiming(), robot_at(r, l_from), False)
 move.add_effect(StartTiming(), is_occupied(l_from), False)
 move.add_effect(EndTiming(), robot_at(r, l_to), True)
-move.add_effect(EndTiming(), is_occupied(l_to), True)
-def decrease_charge_fun(problem, state, actual_params):
+move.add_effect(StartTiming(), is_occupied(l_to), True)
+def fun(problem, state, actual_params):
         #if dist is not constant, use this:
         # dist = state.get_value(distance(actual_params.get(l_from),actual_params.get(l_to))).constant_value()
         dist = LOCATION_DISTANCE
-        requiredCharge = 2 * dist #simulated as some function of distance
+        requiredCharge = (dist)
         currentCharge = state.get_value(charge(actual_params.get(r))).constant_value()
         return [Int(currentCharge-requiredCharge)]
-move.set_simulated_effect(StartTiming(),SimulatedEffect([charge(r)], decrease_charge_fun))
+move.set_simulated_effect(EndTiming(),SimulatedEffect([charge(r)], fun))
 
 pickup = DurativeAction('pickup', p = package, r = robot, l = location)
 p = pickup.parameter('p')
@@ -74,19 +77,41 @@ drop.add_condition(StartTiming(),robot_has_package(p, r))
 drop.add_effect(StartTiming(),robot_has_package(p, r), False)
 drop.add_effect(EndTiming(),location_has_package(p, l), True)
 
-fillcharge = DurativeAction('fillcharge', r = robot, l = location)
-r = fillcharge.parameter('r')
-l = fillcharge.parameter('l')
-fillcharge.set_fixed_duration(CHARGING_TIME)
-fillcharge.add_condition(StartTiming(), location_is_dock(l))
-fillcharge.add_condition(StartTiming(), robot_at(r, l))
-fillcharge.add_effect(EndTiming(), charge(r), FULLTANK)
+dockcharge = DurativeAction('dockcharge', r = robot, l = location)
+r = dockcharge.parameter('r')
+l = dockcharge.parameter('l')
+dockcharge.set_fixed_duration(CHARGING_TIME)
+dockcharge.add_condition(StartTiming(), location_is_dock(l))
+dockcharge.add_condition(StartTiming(), robot_at(r, l))
+dockcharge.add_effect(EndTiming(), charge(r), FULLTANK)
+
+# givecharge = DurativeAction('givecharge', r_giver = robot, l_giver = location, r_taker = robot, l_taker = location)
+# r_giver = dockcharge.parameter('r_giver')
+# r_taker = dockcharge.parameter('r_taker')
+# l_giver = dockcharge.parameter('l_giver')
+# l_taker = dockcharge.parameter('l_taker')
+# givecharge.set_fixed_duration(CHARGING_TIME)
+# givecharge.add_condition(StartTiming(),robot_at(r_giver, l_giver))
+# givecharge.add_condition(StartTiming(),robot_at(r_taker, l_taker))
+# givecharge.add_condition(StartTiming(),Equals(distance(l_giver,l_taker),LOCATION_DISTANCE))
+# givecharge.add_condition(StartTiming(), location_is_dock(l))
+# givecharge.add_condition(StartTiming(), robot_at(r, l))
+# def fun(problem, state, actual_params):
+#         #if dist is not constant, use this:
+#         # dist = state.get_value(distance(actual_params.get(l_from),actual_params.get(l_to))).constant_value()
+#         dist = LOCATION_DISTANCE
+#         requiredCharge = 2 * dist #simulated as some function of distance
+#         currentCharge = state.get_value(charge(actual_params.get(r))).constant_value()
+#         return [Int(currentCharge-requiredCharge)]
+# move.set_simulated_effect(StartTiming(),SimulatedEffect([charge(r)], fun))
+
+
 
 problem = Problem('maildelivery')
 problem.add_action(move)
 problem.add_action(pickup)
 problem.add_action(drop)
-problem.add_action(fillcharge)
+problem.add_action(dockcharge)
 problem.add_fluent(robot_at, default_initial_value = False)
 problem.add_fluent(is_connected, default_initial_value = False)
 problem.add_fluent(is_occupied, default_initial_value = False)
@@ -100,9 +125,9 @@ problem.add_fluent(charge, default_initial_value = 0) #unsolvable problem withou
 # --------------------------------------------- Set specific values
 #objects of problem
 locations = [Object(f"l{i}", location) for i in range(4)]
-deliverybot = Object("r",robot)
+deliverybots = [Object(f"r{i}",robot) for i in range(2)]
 note = Object("p",package)
-problem.add_objects(locations + [deliverybot] + [note])
+problem.add_objects(locations + deliverybots + [note])
 # #initial values of grid locations
 problem.set_initial_value(is_connected(locations[0],locations[1]),True)
 problem.set_initial_value(is_connected(locations[1],locations[2]),True)
@@ -114,23 +139,28 @@ problem.set_initial_value(distance(locations[3],locations[2]),1)
 #connect pump to grid
 problem.set_initial_value(location_is_dock(locations[0]),True)
 #robot at start
-problem.set_initial_value(robot_at(deliverybot,locations[0]),True)
+problem.set_initial_value(robot_at(deliverybots[0],locations[0]),True)
+problem.set_initial_value(robot_at(deliverybots[1],locations[3]),True)
 problem.set_initial_value(is_occupied(locations[0]),True)
+problem.set_initial_value(is_occupied(locations[3]),True)
 #connect houses to grid and place package in house 0
 problem.set_initial_value(location_has_package(note,locations[3]),True)
 #fuel at start
-problem.set_initial_value(charge(deliverybot),1)
+problem.set_initial_value(charge(deliverybots[0]),1)
+problem.set_initial_value(charge(deliverybots[1]),10)
 #goal
 problem.add_timed_goal(StartTiming(10.0), location_has_package(note,locations[2]))
 
-with OneshotPlanner(names=['tamer', 'tamer'],
-                    params=[{'heuristic': 'hadd'}, {'heuristic': 'hmax'}]) as planner:
+import time
+s = time.time()
+with OneshotPlanner(names=['tamer']) as planner:
     result = planner.solve(problem)
-# with OneshotPlanner(problem_kind=problem.kind) as planner:
-#     result = planner.solve(problem)
 
 if result.plan is not None:
         for action in result.plan.timed_actions:
                 print(action[1])
 else:
         print('unable to produce a plan')
+e = time.time()
+
+print(e - s)
