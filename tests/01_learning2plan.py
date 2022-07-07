5a414722e2f44e8f1e22315004c1f0dba677ccea
alonspinner <alonspinner@gmail.com>
2022-07-07 12:40:44 +0300

    updated  01

--- tests/01_learning2plan.py
+++ tests/01_learning2plan.py
@@ -5,11 +5,12 @@ from unified_planning.shortcuts import UserType, BoolType, IntType, Int,\
 unified_planning.shortcuts.get_env().credits_stream = None #removes the printing planners credits 
 from unified_planning.io.pddl_writer import PDDLWriter
 
+from maildelivery import optic_wrapper
+
 # --------------------------------------------- Define problem variables and actions
 #constants (time can be float, but otherwise stick to ints and bools to avoid segmentation faults)
 FULLTANK = 100
-LOCATION_DISTANCE = 1
-MOVE_TIME = 1.0
+DEFAULT_LOCATION_DISTANCE = 1
 PICKUP_TIME = 1.0
 DROP_TIME = 1.0
 CHARGING_TIME = 1.0
@@ -25,7 +26,7 @@ package = UserType('package')
 #problem variables that are changed by actions on objects (no floats please, they cause problems to solvers)
 robot_at = Fluent('robot_at', BoolType(), r = robot, l = location)
 is_connected = Fluent('is_connected', BoolType(), l_from = location, l_to = location)
-is_occupied = Fluent('is_occupied', BoolType(), l = location)
+is_free = Fluent('is_free', BoolType(), l = location)
 distance = Fluent('distance', IntType(), l_from = location, l_to = location)
 delivery_time = Fluent('delivery_time', IntType(), p = package)
 robot_has_package = Fluent('robot_has_package', BoolType(), p = package, r = robot)
@@ -38,23 +39,19 @@ move = DurativeAction('move',  r = robot, l_from = location, l_to = location)
 r = move.parameter('r')
 l_from = move.parameter('l_from')
 l_to = move.parameter('l_to')
-move.set_fixed_duration(MOVE_TIME)
+move.set_fixed_duration(distance(l_from,l_to))
 move.add_condition(StartTiming(),Or(is_connected(l_from, l_to), \
                                     is_connected(l_to, l_from)))
-
-t = distance(l_from,l_to)
-
 move.add_condition(StartTiming(),GE(charge(r),dist2chargeUse(distance(l_from,l_to))))
 move.add_condition(StartTiming(), robot_at(r, l_from))
-move.add_condition(EndTiming(), Not(is_occupied(l_to))) #at end, l_to is free
+move.add_condition(EndTiming(), is_free(l_to))
 move.add_effect(StartTiming(), robot_at(r, l_from), False)
-move.add_effect(StartTiming(), is_occupied(l_from), False)
+move.add_effect(StartTiming(), is_free(l_from), True)
 move.add_effect(EndTiming(), robot_at(r, l_to), True)
-move.add_effect(EndTiming(), is_occupied(l_to), True)
+move.add_effect(EndTiming(), is_free(l_to), False)
 def decrease_charge_fun(problem, state, actual_params):
         #if dist is not constant, use this:
-        # dist = state.get_value(distance(actual_params.get(l_from),actual_params.get(l_to))).constant_value()
-        dist = LOCATION_DISTANCE
+        dist = state.get_value(distance(actual_params.get(l_from),actual_params.get(l_to))).constant_value()
         requiredCharge = dist2chargeUse(dist)
         currentCharge = state.get_value(charge(actual_params.get(r))).constant_value()
         return [Int(currentCharge-requiredCharge)]
@@ -95,8 +92,8 @@ problem.add_action(drop)
 problem.add_action(fillcharge)
 problem.add_fluent(robot_at, default_initial_value = False)
 problem.add_fluent(is_connected, default_initial_value = False)
-problem.add_fluent(is_occupied, default_initial_value = False)
-problem.add_fluent(distance, default_initial_value = LOCATION_DISTANCE)
+problem.add_fluent(is_free, default_initial_value = True)
+problem.add_fluent(distance, default_initial_value = DEFAULT_LOCATION_DISTANCE)
 problem.add_fluent(delivery_time, default_initial_value = 0) #unsolvable problem without initating this value
 problem.add_fluent(robot_has_package, default_initial_value = False)
 problem.add_fluent(location_has_package, default_initial_value = False)
@@ -121,7 +118,7 @@ problem.set_initial_value(distance(locations[3],locations[2]),1)
 problem.set_initial_value(location_is_dock(locations[0]),True)
 #robot at start
 problem.set_initial_value(robot_at(deliverybot,locations[0]),True)
-problem.set_initial_value(is_occupied(locations[0]),True)
+problem.set_initial_value(is_free(locations[0]),False)
 #place packages
 problem.set_initial_value(location_has_package(note,locations[3]),True)
 #fuel at start
@@ -130,23 +127,29 @@ problem.set_initial_value(charge(deliverybot),1)
 # problem.add_timed_goal(StartTiming(10.0), location_has_package(note,locations[2]))
 problem.add_goal(location_has_package(note,locations[2]))
 
-#will only work without timed goal
 w = PDDLWriter(problem)
-print(w.get_domain())
-print(w.get_problem())
+with open(optic_wrapper.DOMAIN_PATH, 'w') as f:
+        print(w.get_domain(), file = f)
+with open(optic_wrapper.PROBLEM_PATH, 'w') as f:
+        print(w.get_problem(), file = f)
+print('copied pddls')
+        
+optic_wrapper.run_optic()
+        
+execution_times, actions, durations = optic_wrapper.get_plan()
 
-print(problem.kind)
+print(actions)
 
 # with OneshotPlanner(names=['tamer', 'tamer'],
 #                     params=[{'heuristic': 'hadd'}, {'heuristic': 'hmax'}]) as planner:
 #     result = planner.solve(problem)
 # with OneshotPlanner(problem_kind=problem.kind) as planner:
 #     result = planner.solve(problem)
-with OneshotPlanner(name='tamer') as planner:
-    result = planner.solve(problem)
+# with OneshotPlanner(name='tamer') as planner:
+#     result = planner.solve(problem)
 
-if result.plan is not None:
-        for action in result.plan.timed_actions:
-                print(action[1])
-else:
-        print('unable to produce a plan')
+# if result.plan is not None:
+#         for action in result.plan.timed_actions:
+#                 print(action[1])
+# else:
+#         print('unable to produce a plan')
