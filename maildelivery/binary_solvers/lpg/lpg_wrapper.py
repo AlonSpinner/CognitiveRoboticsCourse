13c78ac70599f828dc346fb5dfb06a797f17b74a
alonspinner <alonspinner@gmail.com>
2022-07-12 13:50:52 +0300

    lpg now solves better iterativly.. need to debug 16. doesnt work

--- maildelivery/binary_solvers/lpg/lpg_wrapper.py
+++ maildelivery/binary_solvers/lpg/lpg_wrapper.py
@@ -8,19 +8,21 @@ from typing import Literal
 from maildelivery.binary_solvers.paths import DOMAIN_PATH, PROBLEM_PATH, PLAN_PATH, PDDL_DIR
 DIR_PATH = os.path.join(os.path.dirname(__file__))
 BINARY_NAME = "lpg-td"
+LPG_PLAN_PATH = os.path.join(DIR_PATH,'lpg_plan')
+n = 5
 
-
-def run(mode : Literal["speed", "quality", "n 2"] = "speed"):
+def run(mode : Literal["speed", "quality", f"n {n}"] = f"n {n}"):
     p = subprocess.run(f"./{BINARY_NAME} -o {DOMAIN_PATH} -f {PROBLEM_PATH} -{mode} \
-        -seed 1", \
+        -seed 1 -out {LPG_PLAN_PATH}", \
          cwd = DIR_PATH, shell = True)
     found_solution = p.returncode == 0
     if found_solution:
         if mode == "speed" or mode == "quality":
             p = subprocess.run(f"mv plan_problem.pddl.SOL {PLAN_PATH}", cwd = PDDL_DIR, shell = True)
-        elif mode == "n 2":
-            p = subprocess.run(f"mv plan_problem.pddl_1.SOL {PLAN_PATH}", cwd = PDDL_DIR, shell = True)
-            # p = subprocess.run('find . -type f -name "*.SOL"', cwd = PDDL_DIR, shell = True)
+        elif mode == f"n {n}":
+            solfile = LPG_PLAN_PATH + f"_{n}.SOL"
+            p = subprocess.run(f"mv {solfile} {PLAN_PATH}", cwd = PDDL_DIR, shell = True)
+            p = subprocess.run('find . -type f -name "*.SOL" -delete', cwd = DIR_PATH, shell = True)
     return found_solution
 
 def get_plan(file = None):
